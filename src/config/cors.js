488e248047a43906a087daade17ab5a09df0b47f
Alejandro Carcamo <eavc1821@gmail.com>
2026-01-22 11:21:47 -0600

    actualizacion de cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -10,6 +10,12 @@ const originsByEnv = {
   development: [
     "http://localhost:5173",
   ],
+  stagingProd: [
+    "https://control-operativo-grupojd-staging.up.railway.app",
+  ],
+  backendProd: [
+    "https://control-operativo-grupojd-production.up.railway.app",
+  ],
 };
 
 module.exports = originsByEnv[env] || [];
