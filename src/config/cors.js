0af0adffec3d17cc625513656794957081fadfd5
Alejandro Carcamo <eavc1821@gmail.com>
2026-01-22 11:30:41 -0600

    estructura nueva de cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -2,19 +2,20 @@ const env = process.env.NODE_ENV || "development";
 
 const originsByEnv = {
   production: [
+    // Frontend PROD
     "https://control-operativo.gjd78.com",
+    // Backend PROD (para forms HTML)
+    "https://control-operativo-grupojd-production.up.railway.app",
   ],
   staging: [
+    // Frontend STAGING
     "https://co-staging.gjd78.com",
+    // Backend STAGING (HTML aprobaciones)
+    "https://control-operativo-grupojd-staging.up.railway.app",
   ],
   development: [
     "http://localhost:5173",
-  ],
-  stagingProd: [
-    "https://control-operativo-grupojd-staging.up.railway.app",
-  ],
-  backendProd: [
-    "https://control-operativo-grupojd-production.up.railway.app",
+    "http://localhost:3000",
   ],
 };
 
