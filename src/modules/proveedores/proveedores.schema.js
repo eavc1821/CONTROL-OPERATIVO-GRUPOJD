1d0f685fb186f16065f7a7effae33127c1309163
Alejandro Carcamo <eavc1821@gmail.com>
2026-01-29 17:42:42 -0600

    cambios en prov.schema

--- src/modules/proveedores/proveedores.schema.js
+++ src/modules/proveedores/proveedores.schema.js
@@ -1,5 +1,6 @@
 const { z } = require("zod");
-const emptyToNull = (v) => (v === "" ? null : v);
+const emptyToUndefined = (v) => (v === "" ? undefined : v);
+
 
 const createProveedorSchema = z.object({
   nombre: z.string().min(3, "El nombre es obligatorio"),
@@ -9,9 +10,9 @@ const createProveedorSchema = z.object({
   direccion: z.string().optional().nullable(),
   categoria_id: z.number().int().optional().nullable(),
   cai: z.string().min(5, "El CAI es obligatorio"),
-  fecha_limite_emision: z.preprocess(emptyToNull,z.string().regex(/^\d{2}\/\d{2}\/\d{2}$/).nullable()).optional(),
-  rango_factura_desde: z.preprocess(emptyToNull,z.string().regex(/^\d{3}-\d{3}-\d{2}-\d{8}$/).nullable()).optional(),
-  rango_factura_hasta: z.preprocess(emptyToNull,z.string().regex(/^\d{3}-\d{3}-\d{2}-\d{8}$/).nullable()).optional(),
+  fecha_limite_emision: z.preprocess(emptyToUndefined,z.string().regex(/^\d{2}\/\d{2}\/\d{2}$/).optional()),
+  rango_factura_desde: z.preprocess(emptyToUndefined,z.string().regex(/^\d{3}-\d{3}-\d{2}-\d{8}$/).optional()),
+  rango_factura_hasta: z.preprocess(emptyToUndefined,z.string().regex(/^\d{3}-\d{3}-\d{2}-\d{8}$/).optional()),
 });
 
 const updateProveedorSchema = z.object({
@@ -22,9 +23,9 @@ const updateProveedorSchema = z.object({
   direccion: z.string().optional().nullable(),
   categoria_id: z.number().int().optional().nullable(),
   cai: z.string().min(5).optional().nullable(),
-  fecha_limite_emision: z.preprocess(emptyToNull,z.string().regex(/^\d{2}\/\d{2}\/\d{2}$/).nullable()).optional(),
-  rango_factura_desde: z.preprocess(emptyToNull,z.string().regex(/^\d{3}-\d{3}-\d{2}-\d{8}$/).nullable()).optional(),
-  rango_factura_hasta: z.preprocess(emptyToNull,z.string().regex(/^\d{3}-\d{3}-\d{2}-\d{8}$/).nullable()).optional(),
+  fecha_limite_emision: z.preprocess(emptyToUndefined,z.string().regex(/^\d{2}\/\d{2}\/\d{2}$/).optional()),
+  rango_factura_desde: z.preprocess(emptyToUndefined,z.string().regex(/^\d{3}-\d{3}-\d{2}-\d{8}$/).optional()),
+  rango_factura_hasta: z.preprocess(emptyToUndefined,z.string().regex(/^\d{3}-\d{3}-\d{2}-\d{8}$/).optional()),
 });
 
 module.exports = {
