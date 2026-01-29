7afe182d2d80fcf6f3e57df89f706b36839b7f9a
Alejandro Carcamo <eavc1821@gmail.com>
2026-01-29 17:59:40 -0600

    actualizacion de zoad en correo

--- src/modules/proveedores/proveedores.schema.js
+++ src/modules/proveedores/proveedores.schema.js
@@ -6,16 +6,15 @@ const createProveedorSchema = z.object({
   nombre: z.string().min(3, "El nombre es obligatorio"),
   ruc: z.string().optional().nullable(),
   contacto: z.string().optional().nullable(),
-  correo: z.string().email("Correo inválido").optional().nullable(),
+  correo: z.preprocess(emptyToUndefined,z.string().email("Correo inválido").optional()),
   direccion: z.string().optional().nullable(),
   categoria_id: z.preprocess(
-  (v) => {
-    if (v === "" || v === null || v === undefined) return null;
-    return Number(v);
-  },
-  z.number().int().nullable()
-).optional(),
-
+    (v) => {
+      if (v === "" || v === null || v === undefined) return null;
+      return Number(v);
+    },
+    z.number().int().nullable()
+  ).optional(),
   cai: z.string().min(5, "El CAI es obligatorio"),
   fecha_limite_emision: z.preprocess(emptyToUndefined,z.string().regex(/^\d{2}\/\d{2}\/\d{2}$/).optional()),
   rango_factura_desde: z.preprocess(emptyToUndefined,z.string().regex(/^\d{3}-\d{3}-\d{2}-\d{8}$/).optional()),
@@ -26,7 +25,7 @@ const updateProveedorSchema = z.object({
   nombre: z.string().min(3).optional(),
   ruc: z.string().optional().nullable(),
   contacto: z.string().optional().nullable(),
-  correo: z.string().email("Correo inválido").optional().nullable(),
+  correo: z.preprocess(emptyToUndefined,z.string().email("Correo inválido").optional()),
   direccion: z.string().optional().nullable(),
   categoria_id: z.preprocess(
     (v) => {
