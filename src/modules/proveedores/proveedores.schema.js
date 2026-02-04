45585eb6b87c9c88d26a635de5f057b680498bd4
Alejandro Carcamo <eavc1821@gmail.com>
2026-02-04 09:50:50 -0600

    delete a datos fiscales del schema de proveedores

--- src/modules/proveedores/proveedores.schema.js
+++ src/modules/proveedores/proveedores.schema.js
@@ -15,10 +15,6 @@ const createProveedorSchema = z.object({
     },
     z.number().int().nullable()
   ).optional(),
-  cai: z.string().min(5, "El CAI es obligatorio"),
-  fecha_limite_emision: z.preprocess(emptyToUndefined,z.string().regex(/^\d{2}\/\d{2}\/\d{2}$/).optional()),
-  rango_factura_desde: z.preprocess(emptyToUndefined,z.string().regex(/^\d{3}-\d{3}-\d{2}-\d{8}$/).optional()),
-  rango_factura_hasta: z.preprocess(emptyToUndefined,z.string().regex(/^\d{3}-\d{3}-\d{2}-\d{8}$/).optional()),
 });
 
 const updateProveedorSchema = z.object({
@@ -34,10 +30,6 @@ const updateProveedorSchema = z.object({
     },
     z.number().int().nullable()
   ).optional(),
-  cai: z.string().min(5).optional().nullable(),
-  fecha_limite_emision: z.preprocess(emptyToUndefined,z.string().regex(/^\d{2}\/\d{2}\/\d{2}$/).optional()),
-  rango_factura_desde: z.preprocess(emptyToUndefined,z.string().regex(/^\d{3}-\d{3}-\d{2}-\d{8}$/).optional()),
-  rango_factura_hasta: z.preprocess(emptyToUndefined,z.string().regex(/^\d{3}-\d{3}-\d{2}-\d{8}$/).optional()),
 });
 
 module.exports = {
